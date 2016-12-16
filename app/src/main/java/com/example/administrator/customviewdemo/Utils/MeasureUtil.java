6a16a3435763d15a66dd3aa8111646948cad7cfc
jiasheng <918781286@qq.com>
2016-12-16 12:10:46 +0800

    i look look123

--- app/src/main/java/com/example/administrator/customviewdemo/Utils/MeasureUtil.java
+++ app/src/main/java/com/example/administrator/customviewdemo/Utils/MeasureUtil.java
@@ -14,7 +14,7 @@ public final class MeasureUtil {
 	 * ��ȡ��Ļ�ߴ�
 	 * 
 	 * @param activity
-	 *            Activity
+	 *            Activity123
 	 * @return
 	 */
 	public static int[] getScreenSize(Activity activity) {
